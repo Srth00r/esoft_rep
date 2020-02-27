f0bb4151b5d63588715f4ee02dba53baf7be55da
1 <1@303-3>
2020-02-27 17:47:39 +0500

    2777

--- esoft_zhk/esoft/esoft/Form1.cs
+++ esoft_zhk/esoft/esoft/Form1.cs
@@ -53,5 +53,10 @@ from[dbo].[houses_in_complexes] as h1";
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
